d82cf42549436bedc0cbb7c77c00b97c4ed13c76
Igor Golędzinowski <igorgoledzinowski@MacBook-Pro-Igor.local>
2020-07-31 22:50:29 +0200

    some JS improvements

--- js/app.js
+++ js/app.js
@@ -6,165 +6,102 @@ const column = [1, 2, 3, 4, 5];
 const colorNumber = [1, 2, 3, 4, 5];
 let r, g, b;
 
-for (var i = 0; i <= column.length - 1; i++) {
+for (let i = 0; i <= column.length - 1; i++) {
 	column[i] = document.getElementById("col-" + (i + 1));
 	colorNumber[i] = document.getElementById("color-code" + (i + 1));
 }
-let colorArr = [];
 
-let color = {};
+//array of objects
+let colorContainer = [];
 
-(function colorsGenerator() {
-	for (var i = 0; i < column.length; i++) {
-		r = Math.floor(Math.random() * 256);
-		g = Math.floor(Math.random() * 256);
-		b = Math.floor(Math.random() * 256);
-		color = {
-			r: r,
-			g: g,
-			b: b
-		};
-		colorArr.push(color);
-		if (column[i].querySelector('i.locked') != true) {
-			column[i].style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
-			colorNumber[i].value = column[i].style.backgroundColor;
-		}
-		colGen = colorsGenerator;
-	}
-})();
+const randomColor = () => {
+	const random = Math.floor(Math.random() * 256);
+	return random;
+};
 
-console.log(colorArr);
-
-
-function convertToHex(obj){
-	let result = "";
-	while(obj.r % 16 != 0){
-
-	}
-
-	return result; 
+const refresh = (index) => {
+	column[index].style.backgroundColor = `rgb( ${colorContainer[index].rgb[0]}, ${colorContainer[index].rgb[1]}, ${colorContainer[index].rgb[2]})`;
+	colorNumber[index].value = column[index].style.backgroundColor;
 }
 
+(colorGenerator = () => {
+	for (let i = 0; i < column.length; i++) {
+		r = randomColor();
+		g = randomColor();
+		b = randomColor();
 
-function addNewColumn() {
+		if(colorContainer.length < 5 || colorContainer[i].isLocked === false){
+		colorContainer[i] = new Object({
+			name: "",
+			rgb:[r,g,b],
+			hex: `#`,
+			isLocked: false
+			});
+		}
+
+		if (colorContainer[i].isLocked === false) {
+			refresh(i);
+		}
+	}
+	console.log(colorContainer);
+	colGen = colorGenerator;
+})();
+
+const lock = ( value , index ) => {
+	const lockIcon = ['icon-lock-open-alt', 'icon-lock', 'locked'];
+	let result;
+	//loop threw icon classes
+	for(let i=0; i< lockIcon.length; i++){
+		value.classList.toggle(lockIcon[i]);
+	}
+	value.classList.contains("icon-lock-open-alt") ?  result = false : result = true;
+	colorContainer[index].isLocked = result;
+}
+
+const addNewColumn = () => {
 	if (index != 1) {
 		index--;
-		for (var i = 0; i < column.length; i++) {
-			column[i].style.width = width[index] + "%";
-			
+		for (let i = 0; i < column.length; i++) {
+			column[i].style.width = width[index] + `%`;
 		}
-		column[tmp].classList.remove("hide");
+		column[tmp].classList.remove(`hide`);
 		currentWidth = width[index];
 		tmp += 1;
 	}
 }
 
-function removeColumn() {
+const removeColumn = () => {
 	if (index != 5) {
 		index++;
-		for (var i = 0; i < column.length; i++) {
-			column[i].style.width = width[index] + "%";
+		for (let i = 0; i < column.length; i++) {
+			column[i].style.width = width[index] + `%`;
 		}
-		column[tmp - 1].classList.add("hide");
+		column[tmp - 1].classList.add(`hide`);
 		currentWidth = width[index];
 		tmp -= 1;
 	}
-
 }
 
-function swapColorNext(m) {
-	var tempBg, tempTx;
-	//swap columns
-	tempBg = column[m + 1].style.backgroundColor
-	column[m + 1].style.backgroundColor = column[m].style.backgroundColor;
-	column[m].style.backgroundColor = tempBg;
-
-	//swap rgb values
-	tempTx = colorNumber[m + 1].value;
-	colorNumber[m + 1].value = colorNumber[m].value;
-	colorNumber[m].value = tempTx;
-
-	//check if column is locked
-	var isLocked, isLockedFirstCol, isLockedNext;
-
-	isLocked = column[m].querySelectorAll('i')[1].classList.contains("locked");
-	isLockedNext = column[m+1].querySelectorAll('i')[1].classList.contains("locked");
-	isLockedFirstCol = column[m].querySelectorAll('i')[0].classList.contains("locked");
-	
-
-	if(isLocked !== true && isLockedNext){
-		if(m !== 0){
-			lock(column[m].querySelectorAll('i')[1]);
-			lock(column[m+1].querySelector('i.locked'));
-		}else if(m === 0){
-			lock(column[m].querySelectorAll('i')[0]); 
-			lock(column[m + 1].querySelectorAll('i')[1]);
-		}
-	}else if(isLocked && isLockedNext !== true){
-		if(m !== 0){
-			lock(column[m].querySelector('i.locked')); 
-			lock(column[m + 1].querySelectorAll('i')[1]); 
-		}else if(m === 0){
-			lock(column[m].querySelector('i.locked')); 
-			lock(column[m + 1].querySelectorAll('i')[1]);
-		}
-	}else if(isLockedFirstCol && isLockedNext === false){
-		lock(column[m].querySelectorAll('i')[0]);
-		lock(column[m + 1].querySelectorAll('i')[1]);
+const swapColor = ( index , dir ) => {
+	let temp;
+	if(dir === 'next'){
+		temp = colorContainer[index + 1];
+		colorContainer[index + 1] = colorContainer[index];
+		colorContainer[index] = temp;
+		refresh(index+1);
+	}else{
+		temp = colorContainer[index-1];
+		colorContainer[index-1] = colorContainer[index];
+		colorContainer[index] = temp;
+		refresh(index-1);
 	}
-	
+	refresh(index);
 }
 
-function swapColorPrev(value) {
-	var tempBg, tempTx;
-
-	//swap columns
-	tempBg = column[value].style.backgroundColor
-	column[value].style.backgroundColor = column[value - 1].style.backgroundColor;
-	column[value - 1].style.backgroundColor = tempBg;
-
-	//swap rgb values
-	tempTx = colorNumber[value].value;
-	colorNumber[value].value = colorNumber[value - 1].value;
-	colorNumber[value - 1].value = tempTx;	
-
-	isLocked = column[value].querySelectorAll('i')[1].classList.contains("locked");
-
-	isLockedPrev = column[value-1].querySelectorAll('i')[1].classList.contains("locked");
-
-	isLockedFirstCol = column[value].querySelectorAll('i')[0].classList.contains("locked");
-
-	
-	if(isLocked && isLockedPrev === false && value !== 1){
-		lock(column[value].querySelector('i.locked')); 
-		lock(column[value-1].querySelectorAll('i')[1]); 
-	} else if(isLocked === false && isLockedPrev && value !== 1){
-		lock(column[value].querySelectorAll('i')[1]);
-		lock(column[value-1].querySelector('i.locked')); 
-	} else if(isLockedFirstCol === false && isLocked === true && value === 1){
-		lock(column[value].querySelector('i.locked')); 
-		lock(column[value-1].querySelectorAll('i')[0]);
-	} else if(isLockedFirstCol === true && isLocked === false && value === 1){
-		lock(column[value].querySelectorAll('i')[0]);
-		lock(column[value-1].querySelector('i.locked')); 
-	}
-
-
-
-}
-
-
-function lock(value) {
-	var lockIcon = ['icon-lock-open-alt', 'icon-lock', 'locked'];
-	for(var i=0; i< lockIcon.length; i++){
-		value.classList.toggle(lockIcon[i]);
-	}
-}
-
-addEventListener("keyup", function (event) {
+addEventListener("keyup", ( event ) => {
 	if (event.keyCode === 13 || event.which === 13) {
-		for (var i = 0; i < column.length; i++) {
-			if (column[i].querySelector('i.locked')) {
+		for (let i = 0; i < column.length; i++) {
+			if (colorContainer[i].isLocked === true) {
 				colorNumber[i].value = column[i].style.backgroundColor;
 			} else {
 				column[i].style.backgroundColor = colorNumber[i].value;
@@ -173,21 +110,21 @@ addEventListener("keyup", function (event) {
 	}
 });
 
-var saveFileWindow = document.querySelector(".overlay");
+const saveFileWindow = document.querySelector(".overlay");
 
-function windowController(){
+const windowController = () => {
 	saveFileWindow.classList.toggle("show");
 }
 
-function fileData(ext) {
+const fileData = ext => {
 	let data = "";
 	for (i=0; i<colorArr.length; i++){
-		ext ? data += '--color-'+ (i + 1) +": " + colorArr[i]+'\n' : data += colorArr[i]+'\n'
+		ext ? data += `--color- ${(i + 1)} : ${colorArr[i]} \n ` : ` ${data += colorArr[i]} \n`
 	}
-	return "File generated by Color Palette Generator: \n"+data;
+	return `File generated by Color Palette Generator: \n ${data}`;
 }
 
-function exportFile(data, filename, type) {
+const exportFile = (data, filename, type) => {
 	const file = new Blob([data], { type: type });
 	if (window.navigator.msSaveOrOpenBlob) // IE10+
 		window.navigator.msSaveOrOpenBlob(file, filename);
@@ -204,13 +141,3 @@ function exportFile(data, filename, type) {
 		}, 0);
 	}
 }
-
-let removeBtn = document.getElementById('remove-btn');
-let addBtn = document.getElementById('add-btn');
-
-addEventListener("resize", function(){
-	console.log(window.innerWidth);
-	if(window.innerWidth < 700){
-	
-	}
-})
